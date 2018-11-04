df23eb7f06dbccc330102670e66c131aa990ca91
jimuFOmerice <asciiz0x6a696d75@gmail.com>
2018-11-05 01:47:03 +0900

    fix batch file for windows

--- mover.bat
+++ mover.bat
@@ -1,15 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR=C:\shared\135_0308
+set SOURCE_DIR=D:\proj\renamer\canon
 
-set TARGET_DIR=C:\
+set TARGET_DIR=D:\proj\renamer\target
 
 for %%f in ("%SOURCE_DIR%\*") do (
 	echo %%f
 	for /f "usebackq delims=" %%a in (`ruby getExifTime.rb %%f`) do set EXIFDATE=%%a
+	IF NOT EXIST %TARGET_DIR%^\!EXIFDATE! (
+rem		echo %TARGET_DIR%^\!EXIFDATE!
+		mkdir %TARGET_DIR%^\!EXIFDATE!
+	)
 	echo !EXIFDATE!
-	echo move /-Y %%f %TARGET_DIR%!EXIFDATE!
-	pause
+	move /-Y %%f %TARGET_DIR%^\!EXIFDATE!
 rem	exit /b
 )
